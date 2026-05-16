13b1c63204955a89a6da6c6d1b75b3c64194febb
Maulik <pandyamaulik2001@gmail.com>
2026-05-16 11:53:24 +0530

    allowed host

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config("SECRET_KEY", default="unsafe-secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://geovoice.onrender.com/']
+ALLOWED_HOSTS = ['geovoice.onrender.com']
 ALLOWED_HOSTS.extend(
     filter(
         None,
